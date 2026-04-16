ff0216a13a926a0d3bcf5d1dc051a16f0fdc5002
yosephamudapue-ui <yosephamudapue@gmail.com>
2026-04-16 20:38:15 +0700

    Create cekpanggilanusia.py

---
+++ cekpanggilanusia.py
@@ -0,0 +1,9 @@
+umur = int(input("masukkan umur:"))
+if umur <= 5:
+    print("Balita")
+elif umur <= 12:
+    print("Anak-anak")
+elif umur <= 17:
+    print("Remaja")
+elif umur >= 17:
+    print("Dewasa")
